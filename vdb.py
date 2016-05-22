79163c4bdbf1a65cd2a805b33d0af97853aaadd9
Denis <densur@gmail.com>
2016-05-22 16:23:30 +0300

    QMM approach added

--- vdb.py
+++ vdb.py
@@ -1,200 +1,179 @@
+import numpy as np
+from statsmodels import distributions
+from scipy.optimize import fsolve
+from scipy import stats
+from operator import sub
 import math
-import collections
 
 __author__ = 'Denis Surzhko'
-
 
 class LDPortfolio:
     """
     Basic functionality for all LDP calibration facilities.
+    :attribute self.ar: Estimated Accuracy ratio given portfolio distribution and PD values
+    :attribute self.ct: Central Tendency (mean PD) ratio given portfolio distribution and PD values
     """
-    def __init__(self, portf_uncond, rating_type, pd_cond_old=None):
+    def __init__(self, portfolio, rating_type='RATING', pd_cnd=None):
         """
-        :param portf_uncond: Unconditional portfolio distribution from the worst to the best credit quality;
-        :param rating_type: In case RATING, each portf_uncond item contains number of companies in a given rating class;
-                            In case SCORE, each item in the portf_uncond is an exact score;
-        :param pd_cond_old: Current conditional PD distribution from the worst to the best credit quality. Could be used
+        :param portfolio: Unconditional portfolio distribution from the worst to the best credit quality;
+        :param rating_type: In case RATING, each 'portfolio' item contains number of companies in a given rating class;
+                            In case SCORE, each item in the 'portfolio' is an exact score;
+        :param pd_cnd: Current conditional PD distribution from the worst to the best credit quality. Could be used
                             for current AR estimation.
-        :return: Initialized LDPorfolio class.
         """
-        self._portf_uncond = portf_uncond
-        self._rating_type = rating_type
-        self._pd_cond_old = pd_cond_old
-        if pd_cond_old:
-            self._ar_estimate()
+        self.portfolio = np.array(portfolio)
+        self.pd_cnd = np.array(pd_cnd)
+        self.rating_type = rating_type
+        if rating_type == 'RATING':
+            self.portfolio_size = self.portfolio.sum()
+            self.portfolio_dist = self.portfolio.cumsum() / self.portfolio_size
+            self.portfolio_dist = (np.hstack((0, self.portfolio_dist[:-1])) + self.portfolio_dist) / 2
+            self.rating_prob = self.portfolio / self.portfolio_size
         else:
-            self._implied_ar = None
-            self._current_ct = None
-        if rating_type == "RATING":
-            portf_total = sum(portf_uncond)
-            portf_cum = tuple(self._cumulative_sum(portf_uncond))
-            portf_dist = tuple(map(sum, zip(portf_cum, (0,) + portf_cum[:len(portf_cum) - 1])))
-            self._portf_dist = tuple((x / (2*portf_total)) for x in portf_dist)
-        else:
-            portf_total = len(portf_uncond)
-            uniq_scores = collections.Counter(portf_uncond)
-            portf_dist = tuple(uniq_scores[x] / portf_total for x in uniq_scores)
-            self._portf_dist = tuple(self._cumulative_sum(portf_dist))
+            self.portfolio_size = len(self.portfolio)
+            portfolio_ecdf = distributions.ECDF(self.portfolio)
+            self.portfolio_dist = np.minimum(portfolio_ecdf(self.portfolio), 1 - 1.0 / self.portfolio_size)
+            self.rating_prob = np.repeat(1.0 / self.portfolio_size, self.portfolio_size)
+        self.ct = None
+        self.ar = None
+        if not pd_cnd is None:
+            self.ct, self.ar = self._ar_estimate(self.pd_cnd)
 
-    def _ar_estimate(self):
-        portf_total = len(self._portf_uncond)
-        if self._rating_type == 'RATING':
-            rating_prob = [x / portf_total for x in self._portf_uncond]
+    def _ar_estimate(self, pd_cnd):
+        ct = self.rating_prob.T.dot(pd_cnd)
+        ar_1int_1 = self.rating_prob * pd_cnd
+        ar_1int_1 = np.hstack((0, ar_1int_1[:-1]))
+        ar_1int_1 =  (1 - pd_cnd) * self.rating_prob * ar_1int_1.cumsum()
+        ar_1 = 2 * ar_1int_1.sum()
+        ar_2 =  (1 - pd_cnd) * pd_cnd * self.rating_prob * self.rating_prob
+        ar = (ar_1 + ar_2.sum()) * (1.0 / (ct * (1 - ct))) - 1
+        return ct, ar.sum()
+
+class QMM(LDPortfolio):
+    """
+    Calibrates conditional probabilities of default according to Quasi Moment Matching algorithm
+    :attribute self.pd_cnd: calibrated conditional PD (on score/rating)
+    :attribute self.alpha: intercept calibration parameter
+    :attribute self.beta: slope calibration parameter
+    """
+    def __init__(self, portfolio, rating_type = 'RATING', clb_curve = 'robust.logit', portfolio_cnd_no_dft = None):
+        """
+        :param portfolio: Unconditional portfolio distribution from the worst to the best credit quality;
+        :param rating_type: In case RATING, each 'portfolio' item contains number of companies in a given rating class;
+                            In case SCORE, each item in the 'portfolio' is an exact score;
+        :param clb_curve: In case ’logit’, simple logit calibration curve is used (is applicable only for
+                        rating_type = ’SCORE’). In case ’robust.logit’, robust logit function is used
+                        (see Tasche D.(2013) for details).
+        :param portfolio_cnd_no_dft: conditional on no default portfolio distribution (in case None, unconditional
+                                    portfolio distribution is used as a proxy)
+        :return: initialized QMM class object
+        """
+        if rating_type == 'RATING' and clb_curve != 'robust.logit':
+            raise ValueError('Simple logit calibration curve is applicable only for rating.type = \'SCORE\'')
+        super().__init__(portfolio, rating_type, pd_cnd=None)
+        if portfolio_cnd_no_dft is None:
+            self.portfolio_cnd_no_dft = self.portfolio
         else:
-            rating_prob = [1 / portf_total] * portf_total
-        self._current_ct = sum([a*b for a, b in zip(rating_prob, self._pd_cond_old)])
-        ar_1int_1 = [a*b for a, b in zip(self._pd_cond_old, rating_prob)]
-        ar_1int = tuple(self._cumulative_sum([0] + ar_1int_1[:-1]))
-        ar_1 = 2 * sum([(1-a)*b*c for a, b, c in zip(self._pd_cond_old, rating_prob, ar_1int)])
-        ar_2 = sum([(1-a)*a*b*b for a, b in zip(self._pd_cond_old, rating_prob)])
-        self._implied_ar = (ar_1 + ar_2) * (1 / (self._current_ct * (1 - self._current_ct))) - 1
+            self.portfolio_cnd_no_dft = self.portfolio_cnd_no_dft
+        self.clb_curve = clb_curve
+        self.alpha = None
+        self.beta = None
+
+    def fit(self, ct_target, ar_target):
+        """
+        :param ct_target: target Central Tendency
+        :param ar_target: target Accuracy Ratio
+        :return: calibrated QMM class
+        """
+        a = self.__get_pd((0, 0))
+        tf = lambda x: tuple(map(sub, self._ar_estimate(self.__get_pd(x)), (ct_target, ar_target)))
+        params = fsolve(tf, (0, 0))
+        self.alpha, self.beta = params
+        self.pd_cnd = self.__get_pd(params)
+        self.ct, self.ar = self._ar_estimate(self.pd_cnd)
+        return self
+
+    def __get_pd(self, params):
+        if self.clb_curve == 'logit':
+            return self._logit(self.portfolio, params)
+        else:
+            return self._robust_logit(self.portfolio_dist, params)
 
     @staticmethod
-    def _cumulative_sum(values, start=0):
-        for v in values:
-            start += v
-            yield start
+    def _robust_logit(x, params):
+        alpha, beta = params
+        return 1 / (1 + np.exp(- alpha - beta * stats.norm.ppf(x)))
 
-    @property
-    def portf_uncond(self):
-        """
-        :return: unconditional portfolio distribution
-        """
-        return self._portf_uncond
-
-    @property
-    def rating_type(self):
-        """
-        :return: RATING or SCORE type of the rating system
-        """
-        return self._rating_type
-
-    @property
-    def pd_cond_old(self):
-        """
-        :return: Current conditional PD distribution from the worst to the best credit quality.
-        """
-        return self._pd_cond_old
-
-    @property
-    def portf_dist(self):
-        """
-        :return: cumulative non-conditional portfolio distribution
-        """
-        return self._portf_dist
-
-    @property
-    def current_ct(self):
-        """
-        :return: current mean PD in the portfolio (aka central tendency)
-        """
-        return self._current_ct
-
-    @property
-    def implied_ar(self):
-        """
-        :return: current AR estimate based on provided conditional PD and portfolio distribution.
-                 Estimation approach is based on:
-                 Tasche, D. (2013) The art of probability-of-default curve calibration. Journal of Credit Risk, 9:63-103
-        """
-        return self._implied_ar
+    @staticmethod
+    def _logit(x, params):
+        alpha, beta = params
+        return 1 / (1 + np.exp(- alpha - beta * x))
 
 
-class VDBCalibration(LDPortfolio):
+class VDB(LDPortfolio):
     """
-    PD calibration according to Van der Burgt algorithm:
-    Van der Burgt, M. (2008) Calibrating low-default portfolios, using the cumulative accuracy profile.
-    Journal of Risk Model Validation, 1(4):17-33.
+    Calibrates conditional probabilities of default according to Van der Burgt, M. model
+    :attribute self.pd_cnd: calibrated conditional PD (on score/rating)
+    :attribute self.k: intercept calibration parameter
     """
-    def __init__(self, portf_uncond, rating_type, target_ar, target_ct, pd_cond_old=None, pd_uncond_target_ar=0.0):
+    def __init__(self, portfolio, rating_type = 'RATING', pd_on_ar_estimation_sample=0.0):
         """
-        :param portf_uncond: Unconditional portfolio distribution from the worst to the best credit quality;
-        :param rating_type: In case RATING, each portf_uncond item contains number of companies in a given rating class;
-                            In case SCORE, each item in the portf_uncond is an exact score;
-        :param target_ar: Target Accuracy Ratio for calibration purposes;
-        :param target_ct: Target Central Tendency (mean PD in the portfolio) for calibration purposes;
-        :param pd_cond_old: Current conditional PD distribution from the worst to the best credit quality. Could be used
-                            for current AR estimation;
-        :param pd_uncond_target_ar: Unconditional PD of the sample on which AR had been estimated
-                                    (in case is zero, approximation AR = 2*AUC - 1 is used for parameters estimation).
-        :return:
+        :param portfolio: Unconditional portfolio distribution from the worst to the best credit quality;
+        :param rating_type: In case RATING, each 'portfolio' item contains number of companies in a given rating class;
+                            In case SCORE, each item in the 'portfolio' is an exact score;
+        :param pd_on_ar_estimation_sample: mean portfolio PD on AR estimation sample
+        :return: initialized VDB class object
         """
-        super().__init__(portf_uncond, rating_type, pd_cond_old)
-        self._target_ar = target_ar
-        self._target_ct = target_ct
-        self._pd_uncond_target_ar = pd_uncond_target_ar
-        self._k = self._vdb_get_k()
-        self._pd_cond = tuple(target_ct * self._vdb_cap_der(x) for x in self.portf_dist)
+        super().__init__(portfolio, rating_type, pd_cnd=None)
+        self.pd_on_ar_estimation_sample = pd_on_ar_estimation_sample
+
+    def fit(self, ct_target, ar_target):
+        """
+        :param ct_target: target Central Tendency
+        :param ar_target: target Accuracy Ratio
+        :return: calibrated VDB class
+        """
+        self._k = self._vdb_get_k(ar_target)[0]
+        self.pd_cnd = ct_target * self._vdb_cap_der(self.portfolio_dist)
+        self.ct, self.ar = self._ar_estimate(self.pd_cnd)
+        return self
 
     def _vdb_cap_der(self, x):
-        return self._k * math.exp(-self._k * x)/(1 - math.exp(-self._k))
+        return self._k * np.exp(-self._k * x)/(1 - math.exp(-self._k))
 
     def _vdb_ar(self, guess_k):
-        return 2 * (1 / (1 - math.exp(-guess_k)) - 1 / guess_k - 0.5) / (1 - self.pd_uncond_target_ar)
+        return 2.0 * (1.0 / (1.0 - math.exp(-guess_k)) - 1.0 / guess_k - 0.5) / (1.0 - self.pd_on_ar_estimation_sample)
 
-    def _vdb_ar_der(self, guess_k):
-        return 2 * (-math.pow(1 - math.exp(-guess_k), -2) *
-                    math.exp(-guess_k) + math.pow(guess_k, -2)) / (1 - self.pd_uncond_target_ar)
+    def _vdb_get_k(self, ar_target):
+        return fsolve(lambda k: self._vdb_ar(k) - ar_target, 1)
 
-    def _vdb_get_k(self):
-        h = 0.00001
-        last_x = h
-        next_x = last_x + 10*h
-        i = 0
-        while abs(last_x - next_x) > h and i < 1000:
-            last_x = next_x
-            next_x = last_x - (self._vdb_ar(last_x) - self._target_ar) / self._vdb_ar_der(last_x)
-            i += 1
-        return last_x
 
-    @property
-    def target_ar(self):
-        """
-        :return: Target Accuracy Ratio that was used in calibration process
-        """
-        return self._target_ar
 
-    @property
-    def target_ct(self):
-        """
-        :return: Target Central Tendency (mean PD) that was used in calibration process
-        """
-        return self._target_ct
+# Examples
+if __name__ == "__main__":
+    portf_scores = tuple(range(100))
+    pd = [x / 1000 for x in range(100)]
+    pd = [0.1 for x in range(100)]
+    pd = pd[::-1]
+    # Creating portfolio class in order to estimate AR and CT
+    p = LDPortfolio(portf_scores, rating_type='SCORE', pd_cnd=pd)
+    p1 = LDPortfolio([10,20,30,10], rating_type='RATING', pd_cnd=[0.4, 0.3, 0.2, 0.1])
+    # Applying QMM model
+    q = QMM(portf_scores, rating_type='SCORE')
+    q.fit(ct_target=0.1, ar_target=0.3)
+    print(q.alpha)
+    print(q.beta)
+    print(q.pd_cnd)
+    q1 = QMM([10,20,30,10], rating_type='RATING')
+    q1.fit(ct_target=0.1, ar_target=0.3)
+    print(q1.alpha)
+    print(q1.pd_cnd)
+    print(q1.beta)
+    # Applying VDB model
+    v = VDB([50,20,100,10,10], rating_type='RATING', pd_on_ar_estimation_sample = 0.5)
+    v.fit(ct_target=0.1, ar_target=0.5)
+    print(v.pd_cnd)
+    print(v.ct)
+    print(v.ar)
 
-    @property
-    def k(self):
-        """
-        :return: Smooth parameter for CAP  curve.
-        """
-        return self._k
-
-    @property
-    def pd_cond(self):
-        """
-        :return: Calibrated Probabilities of Default
-        """
-        return self._pd_cond
-
-    @property
-    def pd_uncond_target_ar(self):
-        """
-        :return: Unconditional PD of the sample on which AR had been estimated
-                 (in case is zero, approximation AR = 2*AUC - 1 was used for parameters estimation).
-        """
-        return self._pd_uncond_target_ar
-
-# ## Examples
-# portf_scores = tuple(range(100))
-# pd = [x / 1000 for x in range(100)]
-# pd = pd[::-1]
-# p = LDPortfolio(portf_scores, rating_type='SCORE', pd_cond_old=pd)
-# vdb = VDBCalibration(portf_scores, rating_type='SCORE', target_ar=0.5,
-#                      target_ct=0.15, pd_cond_old=pd, pd_uncond_target_ar=0.1)
-# vdb = VDBCalibration([10, 20, 20, 30, 10], rating_type='RATING', target_ar=0.5,
-#                      target_ct=0.15, pd_uncond_target_ar=0.1)
-#
-# print(vdb.k)
-# print(vdb.implied_ar)
-# print(vdb.current_ct)
-# print(vdb.pd_cond)
 
